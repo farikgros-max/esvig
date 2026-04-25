2304f622b3aef0c6e42be7d383baf96144cc009b
farikgros-max <farikgros@gmail.com>
2026-04-25 21:34:16 +0300

    Create states.py

---
+++ states.py
@@ -0,0 +1,31 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class OrderForm(StatesGroup):
+    waiting_for_budget = State()
+    waiting_for_contact = State()
+    waiting_for_deposit_amount = State()
+    processing_deposit = State()
+
+class AddChannelStates(StatesGroup):
+    waiting_for_category = State()
+    waiting_for_name = State()
+    waiting_for_price = State()
+    waiting_for_subscribers = State()
+    waiting_for_url = State()
+    waiting_for_description = State()
+
+class EditChannelStates(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_price = State()
+    waiting_for_subscribers = State()
+    waiting_for_url = State()
+    waiting_for_description = State()
+    waiting_for_category = State()
+
+class AddCategoryStates(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_display_name = State()
+
+class AdminBalanceStates(StatesGroup):
+    waiting_for_user_id = State()
+    waiting_for_amount = State()
